d8bec4c5aad42afb7062cd954ea1aafa4b89e6b0
wybosys <wybosys@gmail.com>
2018-04-13 18:06:21 +0800

    prj

--- app/model/Code.php
+++ app/model/Code.php
@@ -9,9 +9,9 @@ class Code
     const EXCEPTION = -2;
     const PARAMETERS = -3; // 参数错误
     const NEED_AUTH = -4; // 需要登陆
-    const TARGET_NOT_FOUND = -5; // 没有找到制定的对象
+    const TARGET_NOT_FOUND = -5; // 没有找到指定的对象
     const OVERFLOW = -6; // 溢出
-    const SQL_ERROR = -7; // mysql错误
+    const DB_ERROR = -7; // 数据库错误错误
     const TARGET_EXISTS = -8; // 目标已经存在
     const USER_BLOCK = -9; // 账号被锁定
     const NO_USER = -10; // 用户不存在
---
+++ app/model/Db.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Model;
+
+class Db extends \Phalcon\Mvc\Model
+{
+
+    /**
+     * @throws \Exception
+     */
+    public function update($data = null, $whiteList = null)
+    {
+        try {
+            parent::update($data, $whiteList);
+        } catch (\Throwable $exc) {
+            throw new \Exception($exc->getMessage(), Code::DB_ERROR);
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function save($data = null, $whiteList = null)
+    {
+        try {
+            parent::save($data, $whiteList);
+        } catch (\Throwable $exc) {
+            throw new \Exception($exc->getMessage(), Code::DB_ERROR);
+        }
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function delete()
+    {
+        try {
+            parent::delete();
+        } catch (\Throwable $exc) {
+            throw new \Exception($exc->getMessage(), Code::DB_ERROR);
+        }
+    }
+}
