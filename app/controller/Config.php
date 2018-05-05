1c7c19d921ab467c51d2a1cafc199062580c77f0
wybosys <wybosys@gmail.com>
2018-05-05 17:09:06 +0800

    prj

--- app/controller/Config.php
+++ app/controller/Config.php
@@ -2,24 +2,20 @@
 
 namespace App\Controller;
 
-define("CONFIG_IS_LOCAL", Config::IsLocal());
-define("CONFIG_IS_DEVOPS", Config::IsDevops());
-define("CONFIG_IS_DEVOPSRELEASE", Config::IsDevopsRelease());
-
 class Config extends \Phalcon\Config
 {
     static function Use($local, $devops, $devopsrelease)
     {
-        if (CONFIG_IS_DEVOPS)
+        if (self::IsLocal())
             return $local;
-        if (CONFIG_IS_DEVOPS)
+        if (self::IsDevops())
             return $devops;
         return $devopsrelease;
     }
 
     static function IsLocal(): bool
     {
-        return !getenv('DEVOPS') != null && !getenv('DEVOPS_RELEASE') != null;
+        return getenv('DEVOPS') == null && getenv('DEVOPS_RELEASE') == null;
     }
 
     static function IsDevopsRelease(): bool
@@ -29,7 +25,7 @@ class Config extends \Phalcon\Config
 
     static function IsDevops(): bool
     {
-        return !self::IsDevopsRelease() && getenv('DEVOPS') != null;
+        return getenv('DEVOPS_RELEASE') == null && getenv('DEVOPS') != null;
     }
 
 }
