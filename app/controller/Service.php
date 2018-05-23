27f0a9b433eca7dd99c6468703c7b0f249a89db3
wybosys <wybosys@gmail.com>
2018-05-23 20:03:50 +0800

    prj

--- app/controller/Service.php
+++ app/controller/Service.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Phalcon\Http\Request\File;
 use Phalcon\Http\RequestInterface;
-use Test\Model\CidrTest;
 
 class Service
 {
@@ -132,69 +131,8 @@ class Service
 
     static function CidrMatch($ip, $cidr)
     {
-        return (new CIDRmatch())->match($ip, $cidr);
-    }
-}
-
-class CIDRmatch
-{
-    public function match($ip, $cidr)
-    {
         list($subnet, $mask) = explode('/', $cidr);
-        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-            $ipVersion = 'v4';
-        } else {
-            if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-                $ipVersion = 'v6';
-            } else {
-                return false;
-            }
-        }
-        switch ($ipVersion) {
-            case 'v4':
-                return $this->IPv4Match($ip, $subnet, $mask);
-                break;
-            case 'v6':
-                return $this->IPv6Match($ip, $subnet, $mask);
-                break;
-        }
-    }
-
-    private function IPv6MaskToByteArray($subnetMask)
-    {
-        $addr = str_repeat("f", $subnetMask / 4);
-        switch ($subnetMask % 4) {
-            case 0:
-                break;
-            case 1:
-                $addr .= "8";
-                break;
-            case 2:
-                $addr .= "c";
-                break;
-            case 3:
-                $addr .= "e";
-                break;
-        }
-        $addr = str_pad($addr, 32, '0');
-        $addr = pack("H*", $addr);
-        return $addr;
-    }
-
-    private function IPv6Match($address, $subnetAddress, $subnetMask)
-    {
-        $subnet = inet_pton($subnetAddress);
-        $addr = inet_pton($address);
-        $binMask = $this->IPv6MaskToByteArray($subnetMask);
-        return ($addr & $binMask) == $subnet;
-    }
-
-    private function IPv4Match($address, $subnetAddress, $subnetMask)
-    {
-        if ((ip2long($address) & ~((1 << (32 - $subnetMask)) - 1)) == ip2long($subnetAddress)) {
-            return true;
-        }
-        return false;
+        return (ip2long($ip) & ~((1 << (32 - $mask)) - 1)) == ip2long($subnet);
     }
 }
 
