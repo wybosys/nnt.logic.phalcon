6231aacd576804a2199285f3de5af7d489291b17
wybosys <wybosys@gmail.com>
2018-05-29 14:11:19 +0800

    prj

--- app/controller/Api.php
+++ app/controller/Api.php
@@ -181,7 +181,7 @@ class Api extends Controller
 
         // 如果开了缓存，则尝试从缓存中恢复数据
         $cache = null;
-        if ($info->cache && Cache::IsEnabled()) {
+        if ($info->cache) {
             $cache = new Cache();
         }
 
