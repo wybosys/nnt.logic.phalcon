becb58c48d4f9249ff9260409e74bc64ee816df4
wybosys <wybosys@gmail.com>
2018-05-30 17:12:16 +0800

    prj

---
+++ app/controller/Profiler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+// 性能测试
+class Profiler
+{
+    static function IsEnabled(): bool
+    {
+        return extension_loaded('tideways_xhprof');
+    }
+
+    // 开始取样
+    static function Start()
+    {
+        tideways_xhprof_enable();
+    }
+
+    // 结束取样
+    static function Stop()
+    {
+        $data = tideways_xhprof_disable();
+        $name = 'phalcon';
+        if (Config::IsDevops())
+            $name = str_replace('/', '_', getenv('PROJECT'));
+        file_put_contents(
+            sys_get_temp_dir() . "/" . uniqid() . ".$name.xhprof",
+            serialize($data)
+        );
+    }
+}
--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 error_reporting(E_ALL);
 
-if (!isset($_GET['_url']))
-    exit();
+if (!isset($_GET['_url'])) {
+    throw new \Exception("没有按照phalcon的要求设置web服务器的url转发规则");
+}
 
 $phs = array_values(array_filter(explode('/', $_GET['_url'])));
 if (!count($phs))
@@ -11,10 +12,12 @@ if (!count($phs))
 
 // 加载二级模块
 $REDIRECT_MODULE = __DIR__ . '/' . $phs[0] . '/index.php';
-if (!file_exists($REDIRECT_MODULE))
-    exit();
+if (!file_exists($REDIRECT_MODULE)) {
+    throw new \Exception("没有找到模块");
+}
 
 (function () {
     global $REDIRECT_MODULE;
+    // 加载模块
     require $REDIRECT_MODULE;
 })();
