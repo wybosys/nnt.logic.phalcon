b07a3f07be4246447f46a11afdabfe5d3a23cfe5
wybosys <wybosys@gmail.com>
2018-06-12 17:30:53 +0800

    prj

--- test/config/config.php
+++ test/config/config.php
@@ -3,4 +3,4 @@
 defined('MODULE_DIR') || define('MODULE_DIR', dirname(__DIR__) . '/');
 defined('APP_DIR') || define('APP_DIR', dirname(dirname(__DIR__)) . '/');
 
-return include dirname(dirname(__DIR__)) . "/app/config/config.php";
+return include dirname(dirname(__DIR__)) . "/app/config/appconfig.php";
