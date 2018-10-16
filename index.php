0d58ba760842692e85cd142adeffe4e82b3babff
wybosys <wybosys@gmail.com>
2018-10-16 16:02:20 +0800

    prj

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
 <?php
 
-error_reporting(E_ALL);
+error_reporting(E_ERROR);
 
-if (!isset($_GET['_url'])) {
-    // 没有按照phalcon的要求设置web服务器的url转发规则
+// 收集所有的get、post参数
+if (isset($_GET['_url'])) {
+    // 如果存在url，则按照phalcon标准的keypath来路由
+    $phs = array_values(array_filter(explode('/', $_GET['_url'])));
+    if (!count($phs)) {
+        throw new \Exception("传递的url参数错误");
+    }
+
+    // 加载二级模块
+    $REDIRECT_MODULE = __DIR__ . '/' . $phs[0] . '/index.php';
+    if (!file_exists($REDIRECT_MODULE)) {
+        throw new \Exception("没有找到模块");
+    }
+
+    (function () {
+        global $REDIRECT_MODULE;
+        // 加载模块
+        require $REDIRECT_MODULE;
+    })();
+} else if (($action = $_GET['action']) || ($action = $_POST['action'])) {
+    // 按照logic的规则来路由
+    $phs = explode('.', $action);
+
+    // 加载二级模块
+    $REDIRECT_MODULE = __DIR__ . '/' . $phs[0] . '/index.php';
+    if (!file_exists($REDIRECT_MODULE)) {
+        throw new \Exception("没有找到模块");
+    }
+
+    (function () {
+        global $REDIRECT_MODULE;
+        // 加载模块
+        require $REDIRECT_MODULE;
+    })();
+} else {
+    // 所有规则都不满足
     echo "NntLogicPhalcon WebRoot";
     http_response_code(404);
     return;
 }
 
-$phs = array_values(array_filter(explode('/', $_GET['_url'])));
-if (!count($phs)) {
-    throw new \Exception("传递的url参数错误");
-}
-
-// 加载二级模块
-$REDIRECT_MODULE = __DIR__ . '/' . $phs[0] . '/index.php';
-if (!file_exists($REDIRECT_MODULE)) {
-    throw new \Exception("没有找到模块");
-}
-
-(function () {
-    global $REDIRECT_MODULE;
-    // 加载模块
-    require $REDIRECT_MODULE;
-})();
