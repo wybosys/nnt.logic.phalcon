0be7b71381b20231e630f3525f6b3fabc9b68606
wybosys <wybosys@gmail.com>
2018-10-24 17:00:24 +0800

    s2s

--- nnt/controller/Rest.php
+++ nnt/controller/Rest.php
@@ -1,1 +1,104 @@
+<?php
 
+namespace Nnt\Controller;
+
+use Nnt\Model\Logic;
+use Nnt\Model\HttpMethod;
+use Phalcon\Http\Request\File;
+
+class Rest extends Session
+{
+    static function Fetch(Logic &$m)
+    {
+        $url = "";
+        if ($m->host)
+            $url .= $m->host;
+        $url .= $m->requestUrl();
+        if (strpos($url, '?') == -1)
+            $url .= "?/";
+
+        // 所有请求参数
+        $params = $m->requestParams();
+
+        // 从params里提取出文件
+        $files = [];
+        foreach ($params->fields as $key => $val) {
+            if ($val instanceof File) {
+                $files[$key] = $val;
+                unset($params->fields[$key]);
+            }
+        }
+        if (count($files))
+            $m->method = HttpMethod::$POST;
+
+        // 根据post和get分别处理
+        if ($m->method == HttpMethod::$GET) {
+            $p = [];
+            foreach ($params->fields as $k => $f) {
+                $p[] = $k . "=" . urlencode($f);
+            }
+
+            // 添加permission的控制
+            if (Service::PermissionEnabled()) {
+                $p[KEY_PERMISSIONID] = Service::PermissionId();
+            }
+
+            // 添加跳过的标记
+            if (!Config::IsDevopsRelease()) {
+                $p[KEY_SKIPPERMISSION] = 1;
+            }
+
+            if (count($p))
+                $url .= "&" . implode('&', $p);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $resp = curl_exec($ch);
+            curl_close($ch);
+        } else {
+            $p = [];
+
+            // 添加permission的控制
+            if (Service::PermissionEnabled()) {
+                $p[KEY_PERMISSIONID] = Service::PermissionId();
+            }
+
+            // 添加跳过的标记
+            if (!Config::IsDevopsRelease()) {
+                $p[KEY_SKIPPERMISSION] = 1;
+            }
+
+            if (count($p))
+                $url .= "&" . implode('&', $p);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                "Content-Type: multipart/form-data"
+            ]);
+
+            // 传输文件
+            $data = [];
+            if (count($files)) {
+                foreach ($files as $key => $file) {
+                    $data[$key] = curl_file_create($file->getTempName(), $file->getType(), $file->getName());
+                }
+            }
+
+            // 传输普通参数
+            foreach ($params->fields as $k => $f) {
+                $data[$k] = $f;
+            }
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            $resp = curl_exec($ch);
+            curl_close($ch);
+        }
+
+        // 处理获得的数据
+    }
+}
--- nnt/model/Logic.php
+++ nnt/model/Logic.php
@@ -18,6 +18,27 @@ class HttpContentType
     static $XML = 3;
 }
 
+class RequestParams
+{
+    // 参数列表
+    public $fields = [];
+
+    // 如果是xml这种树形参数，根节点名称
+    public $root = "root";
+}
+
+class ResponseData
+{
+    // 响应的code
+    public $code;
+
+    // 内容类型
+    public $type;
+
+    // 返回的所有数据
+    public $body;
+}
+
 abstract class Logic
 {
     function __construct()
@@ -27,9 +48,8 @@ abstract class Logic
         $this->responseType = HttpContentType::$JSON;
     }
 
-    // 请求的服务器地址
-
     /**
+     * 请求的服务器地址
      * @var string
      */
     public $host;
@@ -66,6 +86,27 @@ abstract class Logic
 
     // 组装请求的url
     abstract function requestUrl(): string;
+
+    // 组装请求的参数集
+    function requestParams(): RequestParams
+    {
+        $t = Proto::Input($this);
+        $r = new RequestParams();
+
+        // 先设置额外的参数，使得徒手设置的参数优先级最高
+        if ($this->additionParams) {
+            foreach ($this->additionParams as $k => $v)
+                $r->fields[$k] = $v;
+        }
+
+        // 设置徒手参数
+        foreach ($t as $key => $val) {
+            if ($val == null)
+                continue;
+            $r->fields[$key] = $val;
+        }
+        return $r;
+    }
 
     static function NewRequest($req)
     {
