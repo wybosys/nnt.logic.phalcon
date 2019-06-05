f52df1e7ab8c8b0b68dc93c714fa80d8347a5504
wybosys <wybosys@gmail.com>
2019-06-05 11:01:05 +0800

    fix

--- test/index.php
+++ test/index.php
@@ -1,31 +1,35 @@
 <?php
-
+    
 use Phalcon\Loader;
 use Phalcon\Db\Adapter\Pdo\Factory as DbFactory;
-use Phalcon\Session\Factory as SesFactory;
-
+    
 define('MODULE_DIR', __DIR__ . '/');
 define('APP_DIR', dirname(__DIR__) . '/');
-
+   
 $loader = new Loader();
 $loader->registerNamespaces([
+    'Test' => APP_DIR . 'test',
+    'Test\Db' => APP_DIR . 'test\db',
+    'Test\Model' => APP_DIR . 'test\model',
+    'Test\Controller' => APP_DIR . 'test\controller',
     'Nnt' => APP_DIR . 'nnt',
-    'Nnt\Model' => APP_DIR . 'nnt/model',
-    'Nnt\Controller' => APP_DIR . 'nnt/controller',
-    'Test' => MODULE_DIR,
-    'Test\Controller' => MODULE_DIR . 'controller',
-    'Test\Model' => MODULE_DIR . 'model',
-    'Test\Db' => MODULE_DIR . 'db'
+    'Nnt\Core' => APP_DIR . 'nnt\core',
+    'Nnt\Store' => APP_DIR . 'nnt\store',
+    'Nnt\Sdks' => APP_DIR . 'nnt\sdks',
+    'Nnt\Db' => APP_DIR . 'nnt\db',
+    'Nnt\Model' => APP_DIR . 'nnt\model',
+    'Nnt\Controller' => APP_DIR . 'nnt\controller'
 ]);
-
+    
 $loader->registerDirs([
     MODULE_DIR . 'controller',
-    MODULE_DIR . "model"
+    MODULE_DIR . 'model'
 ]);
+
 $loader->register();
 
 $di = new \Nnt\Controller\Factory();
-
+    
 $di->setShared('config', function () {
     return include 'config/config.php';
 });
@@ -34,15 +38,9 @@ $di->setShared('db', function () {
     return DbFactory::load($this->getConfig()->database);
 });
 
-$di->setShared('session', function () {
-    $hdl = SesFactory::load($this->getConfig()->session);
-    $hdl->start();
-    return $hdl;
-});
-
 $di->setShared('redis', function () {
-    return new \Nnt\Controller\KvRedis($this->getConfig()->redis);
+    return new \Nnt\Store\KvRedis($this->getConfig()->redis);
 });
-
+    
 $app = new \Test\Controller\Application($di);
 $app->run();
