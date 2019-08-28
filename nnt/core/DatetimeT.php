b9496eac1233538354358c5f3f80c3304097671a
wybosys <wybosys@gmail.com>
2019-08-28 19:24:43 +0800

    time"

--- nnt/core/DatetimeT.php
+++ nnt/core/DatetimeT.php
@@ -4,13 +4,23 @@ namespace Nnt\Core;
 
 class DatetimeT
 {
-    public static function Present(): string
+    static function Present(): string
     {
         return date('Y-m-d H:i:s', time());
+    }
+
+    static function Format(string $fmt, $seconds): string
+    {
+        return date($fmt, $seconds);
     }
 
     static function Now(): int
     {
         return time();
+    }
+
+    static function Current(): float
+    {
+        return gettimeofday(true);
     }
 }
